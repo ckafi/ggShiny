9003687058d79afea20eb8eb971405bca8eb2c2e
Tobias Frilling <tobias@frilling-online.de>
2018-06-13 17:05:30 +0200

    Run 'styler'

--- server.R
+++ server.R
@@ -1,113 +1,141 @@
 library(ggplot2)
 
 server <- function(input, output) {
+  output$contents <- renderTable({
+    inputData()
+  })
 
-    output$contents <- renderTable({inputData()})
+  output$aes <- renderUI({
+    dispatch_aes_ui()
+  })
 
-    output$aes <- renderUI({dispatch_aes_ui()})
+  output$plot <- renderPlot({
+    plot_()
+  })
 
-    output$plot <- renderPlot({plot_()})
+  output$downPlot <- downloadHandler(
+    filename = function() {
+      paste(
+        if (input$title == "") {
+          "plot"
+        } else {
+          input$title
+        } ,
+        ".png",
+        sep = ""
+      )
+    },
+    content = function(file) {
+      plot_()
+      ggsave(file, device = "png")
+    },
+    contentType = "image/png"
+  )
 
-    output$downPlot <- downloadHandler(
-        filename = function() {
-            paste(
-                if(input$title == "") "plot"
-                else input$title,
-                ".png", sep="")
-        },
-        content = function(file) {
-            plot_()
-            ggsave(file, device="png")
-        },
-        contentType = "image/png"
+  inputData <- reactive({
+    req(input$file)
+    read.csv(input$file$datapath,
+      header = input$header,
+      sep = input$sep,
+      quote = input$quote
+    )
+  })
+
+  plot_ <- reactive({
+    ggplot(inputData()) +
+      labs(
+        title = input$title,
+        x = input$xlab,
+        y = input$ylab
+      ) +
+      dispatch_geom_plot() +
+      theme_linedraw() +
+      theme_elements()
+  })
+
+  theme_elements <- function() {
+    return(
+      switch(input$xgrid,
+        "all" = theme(),
+        "major" = theme(panel.grid.minor.x = element_blank()),
+        "none" = theme(
+          panel.grid.minor.x = element_blank(),
+          panel.grid.major.x = element_blank()
         )
-
-    inputData <- reactive({
-        req(input$file)
-        read.csv(input$file$datapath,
-            header = input$header,
-            sep = input$sep,
-            quote = input$quote)
-    })
-
-    plot_ <- reactive({
-        ggplot(inputData()) +
-            labs(title = input$title,
-                x = input$xlab,
-                y = input$ylab) +
-            dispatch_geom_plot() +
-            theme_linedraw() +
-            theme_elements()
-
-    })
-
-    theme_elements <- function() {
-        return(
-            switch(input$xgrid,
-                "all" = theme(),
-                "major" = theme(panel.grid.minor.x = element_blank()),
-                "none" = theme(panel.grid.minor.x = element_blank(),
-                    panel.grid.major.x = element_blank())
-                ) +
-            switch(input$ygrid,
-                "all" = theme(),
-                "major" = theme(panel.grid.minor.y = element_blank()),
-                "none" = theme(panel.grid.minor.y = element_blank(),
-                    panel.grid.major.y = element_blank())
-                )
-            )
-    }
+      ) +
+        switch(input$ygrid,
+          "all" = theme(),
+          "major" = theme(panel.grid.minor.y = element_blank()),
+          "none" = theme(
+            panel.grid.minor.y = element_blank(),
+            panel.grid.major.y = element_blank()
+          )
+        )
+    )
+  }
 
 
-    dispatch_aes_ui <- function() {
-        return(switch(input$plotselect,
+  dispatch_aes_ui <- function() {
+    return(switch(input$plotselect,
 
-                "point" = tagList(
-                    selectInput("x", "Select x",
-                        choices = names(inputData())),
-                    selectInput("y", "Select y",
-                        choices = names(inputData())),
-                    selectInput("shape","Shape",
-                        choices = 0:25,
-                        selected="19"),
-                    sliderInput("size", "Size",
-                        min = 0.1 , max = 5,
-                        value = 1, step = 0.1),
-                    sliderInput("stroke", "Stroke",
-                        min = 0.1 , max = 5,
-                        value = 1, step = 0.1)
-                    ),
+      "point" = tagList(
+        selectInput("x", "Select x",
+          choices = names(inputData())
+        ),
+        selectInput("y", "Select y",
+          choices = names(inputData())
+        ),
+        selectInput("shape", "Shape",
+          choices = 0:25,
+          selected = "19"
+        ),
+        sliderInput("size", "Size",
+          min = 0.1, max = 5,
+          value = 1, step = 0.1
+        ),
+        sliderInput("stroke", "Stroke",
+          min = 0.1, max = 5,
+          value = 1, step = 0.1
+        )
+      ),
 
-                "histogram" = tagList(
-                    selectInput("x", "Select x",
-                        choices = names(inputData()),
-                        selected = input$x),
+      "histogram" = tagList(
+        selectInput("x", "Select x",
+          choices = names(inputData()),
+          selected = input$x
+        ),
 
-                    if(!is.factor(inputData()[[input$x]])) {
-                        m <- max(inputData()[[input$x]])
-                        sliderInput("binwidth", "Size of bins",
-                            min = 0 , max = m,
-                            value = m / 10, step = m / 100)
-                        })
+        if (!is.factor(inputData()[[input$x]])) {
+          m <- max(inputData()[[input$x]])
+          sliderInput("binwidth", "Size of bins",
+            min = 0, max = m,
+            value = m / 10, step = m / 100
+          )
+        }
+      )
+    ))
+  }
 
-                    ))
-    }
 
-    
-    dispatch_geom_plot <- function() {
-        return(switch(input$plotselect,
+  dispatch_geom_plot <- function() {
+    return(switch(input$plotselect,
 
-                "point" = geom_point(
-                    aes(x = get(input$x), y = get(input$y)),
-                    shape = strtoi(input$shape),
-                    size = input$size,
-                    stroke = input$stroke), 
+      "point" = geom_point(
+        aes(x = get(input$x), y = get(input$y)),
+        shape = strtoi(input$shape),
+        size = input$size,
+        stroke = input$stroke
+      ),
 
-                "histogram" = geom_histogram(
-                    aes(x = get(input$x)),
-                    stat = if(is.factor(inputData()[[input$x]]))
-                        { "count" } else { "bin"},
-                    binwidth = input$binwidth)
-                ))
-    }
+      "histogram" = geom_histogram(
+        aes(x = get(input$x)),
+        stat = if (is.factor(inputData()[[input$x]])) {
+          "count"
+        } else {
+          "bin"
+        },
+        binwidth = input$binwidth
+      )
+    ))
+  }
 }
--- ui.R
+++ ui.R
@@ -1,83 +1,115 @@
 library(shinythemes)
 
-filepanel <- tabPanel("File Upload",
-    sidebarLayout(
-        sidebarPanel(
-            fileInput("file", "Choose CSV File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                    "text/comma-separated-values",
-                    "text/plain",
-                    ".csv")),
-
-            tags$hr(),
-
-            checkboxInput("header", "Header", TRUE),
-
-            radioButtons("sep", "Separator",
-                choices = c(Comma = ",",
-                    Semicolon = ";",
-                    Tab = "\t"),
-                selected = ","),
-
-            radioButtons("quote", "Quote",
-                choices = c(None = "",
-                    "Double Quote" = '"',
-                    "Single Quote" = "'"),
-                selected = '"')
-
-            ),
-
-        mainPanel(
-            tableOutput("contents")
-            )
+filepanel <- tabPanel(
+  "File Upload",
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Choose CSV File",
+        multiple = TRUE,
+        accept = c(
+          "text/csv",
+          "text/comma-separated-values",
+          "text/plain",
+          ".csv"
         )
+      ),
+
+      tags$hr(),
+
+      checkboxInput("header", "Header", TRUE),
+
+      radioButtons("sep", "Separator",
+        choices = c(
+          Comma = ",",
+          Semicolon = ";",
+          Tab = "\t"
+        ),
+        selected = ","
+      ),
+
+      radioButtons("quote", "Quote",
+        choices = c(
+          None = "",
+          "Double Quote" = '"',
+          "Single Quote" = "'"
+        ),
+        selected = '"'
+      )
+    ),
+
+    mainPanel(
+      tableOutput("contents")
     )
+  )
+)
 
-plotpanel <- tabPanel("Plot",
-    sidebarLayout(
-        sidebarPanel(
-            tabsetPanel(
-                tabPanel("Main options",
-                    selectInput("plotselect", "Select Plot",
-                        choices = list(
-                            "Scatter plot" = "point",
-                            "Histogram" = "histogram")),
-                    textInput("title", label = "Title:",
-                        value = "Title"),
-                    textInput("xlab", label = "x-Label:",
-                        value = "x-Label"),
-                    textInput("ylab", label = "y-Label:",
-                        value = "y-Label"),
-                    selectInput("xgrid", label = "Vertical Grid Lines",
-                        choices = list(
-                            "All" = "all",
-                            "Only major" = "major",
-                            "None" = "none")),
-                    selectInput("ygrid", label = "Horizontal Grid Lines",
-                        choices = list(
-                            "All" = "all",
-                            "Only major" = "major",
-                            "None" = "none")),
-
-                    tags$hr()),
-
-                tabPanel("Plot specific options",
-                    uiOutput("aes"))
-            )),
-
-        mainPanel(
-            plotOutput('plot'),
-            downloadButton(
-                outputId = "downPlot",
-                label = "Download")
+plotpanel <- tabPanel(
+  "Plot",
+  sidebarLayout(
+    sidebarPanel(
+      tabsetPanel(
+        tabPanel(
+          "Main options",
+          selectInput("plotselect", "Select Plot",
+            choices = list(
+              "Scatter plot" = "point",
+              "Histogram" = "histogram"
             )
+          ),
+          textInput("title",
+            label = "Title:",
+            value = "Title"
+          ),
+          textInput("xlab",
+            label = "x-Label:",
+            value = "x-Label"
+          ),
+          textInput("ylab",
+            label = "y-Label:",
+            value = "y-Label"
+          ),
+          selectInput("xgrid",
+            label = "Vertical Grid Lines",
+            choices = list(
+              "All" = "all",
+              "Only major" = "major",
+              "None" = "none"
+            )
+          ),
+          selectInput("ygrid",
+            label = "Horizontal Grid Lines",
+            choices = list(
+              "All" = "all",
+              "Only major" = "major",
+              "None" = "none"
+            )
+          ),
+
+          tags$hr()
+        ),
+
+        tabPanel(
+          "Plot specific options",
+          uiOutput("aes")
         )
+      )
+    ),
+
+    mainPanel(
+      plotOutput("plot"),
+      downloadButton(
+        outputId = "downPlot",
+        label = "Download"
+      )
     )
+  )
+)
 
 
 
-ui <- navbarPage(title="ggShiny",
-    theme = shinytheme("yeti"),
-    filepanel,
-    plotpanel)
+ui <- navbarPage(
+  title = "ggShiny",
+  theme = shinytheme("yeti"),
+  filepanel,
+  plotpanel
+)
