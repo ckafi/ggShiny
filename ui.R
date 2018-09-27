9e64cf540c49062016ad3a7aae7646a0a858ef0c
Tobias Frilling <tobias@frilling-online.de>
2018-09-27 14:01:27 +0200

    Add comments in ui.R

--- ui.R
+++ ui.R
@@ -1,141 +1,200 @@
+# This file defines the interface of the app. It should only contain markup.
+# Any logic should be put into server.R
+# Shiny defines the interface through nested layout components ("panels" and
+# "widgets). The root of this tree is the 'ui' variable, which can be found at
+# the end of this file.
+
 library(shinythemes)
 library(colourpicker)
 
+# The panel for file upload and data preview
 filepanel <- tabPanel(
-  "File Upload",
+  title = "File Upload",
+  # A sidebarLayout consists of a sidebar and a main area
   sidebarLayout(
+
+    # The sidebar for file upload
     sidebarPanel(
-      checkboxInput("example_data", "Use Example", FALSE),
-      fileInput("file", "Choose CSV File",
+      checkboxInput(
+        inputId = "example_data",
+        label   = "Use Example",
+        value   = FALSE
+      ),
+      # fileInput only handles the _upload_ of the file; the interpretation of
+      # its contents happens in server.R
+      fileInput(
+        inputId  = "file",
+        label    = "Choose CSV File",
         multiple = TRUE,
-        accept = c(
-          "text/csv",
-          "text/comma-separated-values",
-          "text/plain",
-          ".csv"
-        )
+        accept   = c("text/csv",
+                     "text/comma-separated-values",
+                     "text/plain",
+                     ".csv")
       ),
 
       tags$hr(),
 
-      checkboxInput("header", "Header", TRUE),
+      checkboxInput(
+        inputId = "header",
+        label   = "Header",
+        value   = TRUE
+      ),
 
-      radioButtons("sep", "Separator",
-        choices = c(
-          Comma = ",",
-          Semicolon = ";",
-          Tab = "\t"
-        ),
+      radioButtons(
+        inputId  = "sep",
+        label    = "Separator",
+        choices  = c(Comma     = ",",
+                     Semicolon = ";",
+                     Tab       = "\t"),
         selected = ","
       ),
 
-      radioButtons("quote", "Quote",
-        choices = c(
-          None = "",
-          "Double Quote" = '"',
-          "Single Quote" = "'"
-        ),
+      radioButtons(
+        inputId  = "quote",
+        label    = "Quote",
+        choices  = c(None          = "",
+                    "Double Quote" = '"',
+                    "Single Quote" = "'"),
         selected = '"'
       )
     ),
 
+    # the main area with the interpreted content of the uploaded file
     mainPanel(
-      tableOutput("contents")
+      tableOutput(outputId = "contents")
     )
   )
 )
 
-plotpanel <- tabPanel(
-  "Plot",
-  sidebarLayout(
-    sidebarPanel(
-      tabsetPanel(
-        id = "settings",
 
+# the panel for plot selection and display
+plotpanel <- tabPanel(
+  title = "Plot",
+  # A sidebarLayout consists of a sidebar and a main area
+  sidebarLayout(
+
+    sidebarPanel(
+      # the sidebar consists of multiple tabs (a "tabset")
+      tabsetPanel(
+        id = "settings", # id needed for testing
+
+        # the panel for plot selection and plot options
         tabPanel(
-          "Main Options",
-          selectInput("plotselect", "Select Plot",
-            choices = list(
-              "Scatter plot" = "point",
-              "Histogram" = "histogram",
-              "Line Graph" = "line",
-              "Boxplot" = "boxplot",
-              "Heatmap" = "heatmap"
-            )
+          title = "Main Options",
+          selectInput(
+            inputId = "plotselect",
+            label   = "Select Plot",
+            choices = list("Scatter plot" = "point",
+                           "Histogram"    = "histogram",
+                           "Line Graph"   = "line",
+                           "Boxplot"      = "boxplot",
+                           "Heatmap"      = "heatmap")
           ),
           tags$hr(),
-          uiOutput("aes")
+          # the UI elements for the plot options are dynamically created in
+          # server.R
+          uiOutput(outputId = "aes")
         ),
+
+        # the panel for plot colors and labels
         tabPanel(
-          "Labeling and colour(s)",
-          textInput("title",
-            label = "Title:",
-            value = "Title"
+          title = "Labeling and colour(s)",
+          textInput(
+            inputId = "title",
+            label   = "Title:",
+            value   = "Title"
           ),
-          textInput("xlab",
-            label = "x-Label:",
-            value = "x-Label"
+          textInput(
+            inputId = "xlab",
+            label   = "x-Label:",
+            value   = "x-Label"
           ),
-          textInput("ylab",
+          textInput(
+            inputId = "ylab",
             label = "y-Label:",
             value = "y-Label"
           ),
-          checkboxInput("rotate_labels",
-            label = "Rotate tick labels on x-axis",
-            value = FALSE
+          checkboxInput(
+            inputId = "rotate_labels",
+            label   = "Rotate tick labels on x-axis",
+            value   = FALSE
           ),
           tags$hr(),
-          selectInput("xgrid",
-            label = "Vertical Grid Lines",
-            choices = list(
-              "All" = "all",
-              "Only major" = "major",
-              "None" = "none"
-            )
+          selectInput(
+            inputId = "xgrid",
+            label   = "Vertical Grid Lines",
+            choices = list("All" = "all",
+                           "Only major" = "major",
+                           "None" = "none")
           ),
-          selectInput("ygrid",
-            label = "Horizontal Grid Lines",
-            choices = list(
-              "All" = "all",
-              "Only major" = "major",
-              "None" = "none"
-            )
+          selectInput(
+            inputId = "ygrid",
+            label   = "Horizontal Grid Lines",
+            choices = list("All" = "all",
+                           "Only major" = "major",
+                           "None" = "none")
           ),
           tags$hr(),
-          colourInput("col1", "Select primary colour", "black"),
-          colourInput("col2", "Select secondary colour (for some plots)", "white")
+          colourInput(
+            inputId = "col1",
+            label   = "Select primary colour",
+            value   = "black"),
+          colourInput(
+            inputId = "col2",
+            label   = "Select secondary colour (for some plots)",
+            value   = "white")
         ),
+
+        # the panel for the reference line
         tabPanel(
-          "Reference Line",
-          checkboxInput("draw_ref_line",
-            label = "Draw reference line",
-            value = FALSE
+          title = "Reference Line",
+          checkboxInput(
+            inputId = "draw_ref_line",
+            label   = "Draw reference line",
+            value   = FALSE
           ),
-          selectInput("ref_line", "Select Reference Line",
-            choices = list(
-              "Vertical Line" = "vline",
-              "Horizontal Line" = "hline",
-              "AB-Line" = "abline"
-            )
+          selectInput(
+            inputId = "ref_line",
+            label   = "Select Reference Line",
+            choices = list("Vertical Line"   = "vline",
+                           "Horizontal Line" = "hline",
+                           "AB-Line"         = "abline")
           ),
-          numericInput("interc", label = "Intercept", value = 0),
-          numericInput("slope", label = "Slope (for AB-line)", value = 0),
-          selectInput("ref_linetype", "Linetype",
-            choices = c(
-              "solid", "dashed", "dotted",
-              "dotdash", "longdash", "twodash"
-            ),
+          numericInput(
+            inputId = "interc",
+            label   = "Intercept",
+            value   = 0
+          ),
+          numericInput(
+            inputId = "slope",
+            label   = "Slope (for AB-line)",
+            value   = 0
+          ),
+          selectInput(
+            inputId  = "ref_linetype",
+            label    = "Linetype",
+            choices  = c("solid", "dashed", "dotted",
+                         "dotdash", "longdash", "twodash"),
             selected = "solid"
           ),
-          sliderInput("refsize", "Size",
-            min = 0.1, max = 5,
-            value = 0.5, step = 0.1
+          sliderInput(
+            inputId = "refsize",
+            label   = "Size",
+            value   = 0.5,
+            min     = 0.1,
+            max     = 5,
+            step    = 0.1
           ),
-          colourInput("refcol", "Color", "black")
+          colourInput(
+            inputId = "refcol",
+            label   = "Color",
+            value   = "black")
         )
       )
     ),
 
+    # the main area with the generated plot
+    # and the download button
     mainPanel(
       plotOutput("plot"),
       downloadButton(
@@ -147,11 +206,13 @@ plotpanel <- tabPanel(
 )
 
 
-
+# This is the main entry point for the UI
+# navbarPage creates a page with a top level navigation bar
 ui <- navbarPage(
-  id = "main",
+  id = "main", # the id is necessary for testing
   title = "ggShiny",
   theme = shinytheme("yeti"),
+  # the panels
   filepanel,
   plotpanel
 )
